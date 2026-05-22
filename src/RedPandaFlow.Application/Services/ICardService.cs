da342e25b97772fc1050b54275778f9d9e97ab2d
Torolgo <ylan.dessenne.spam@gmail.com>
2026-05-22 19:29:26 +0200

    feat(cards): implement ICardService and CardService for card management

---
+++ src/RedPandaFlow.Application/Services/ICardService.cs
@@ -0,0 +1,16 @@
+using RedPandaFlow.Application.Common;
+using RedPandaFlow.Application.DTOs;
+
+namespace RedPandaFlow.Application.Services
+{
+    public interface ICardService
+    {
+        Task<ServiceResult<List<CardDto>>> GetCardsByBoardIdAsync(Guid workspaceId, Guid boardId, Guid userId);
+        Task<ServiceResult<List<CardDto>>> GetCardsByColumnIdAsync(Guid workspaceId, Guid boardId, Guid columnId, Guid userId);
+        Task<ServiceResult<CardDto>> GetCardByIdAsync(Guid workspaceId, Guid boardId, Guid columnId, Guid cardId, Guid userId);
+        Task<ServiceResult<CardDto>> CreateCardAsync(Guid workspaceId, Guid boardId, Guid columnId, Guid userId, CreateCardRequest request);
+        Task<ServiceResult<CardDto>> UpdateCardAsync(Guid workspaceId, Guid boardId, Guid columnId, Guid cardId, Guid userId, UpdateCardRequest request);
+        Task<ServiceResult<bool>> UpdateCardOrderAsync(Guid workspaceId, Guid boardId, Guid columnId, Guid cardId, Guid userId, UpdateCardOrderRequest request);
+        Task<ServiceResult<bool>> DeleteCardAsync(Guid workspaceId, Guid boardId, Guid columnId, Guid cardId, Guid userId);
+    }
+}
