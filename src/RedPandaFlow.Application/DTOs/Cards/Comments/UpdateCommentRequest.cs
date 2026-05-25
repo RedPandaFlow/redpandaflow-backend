c162a2867b75a3b715155ec2e3338990c025e07e
zoom26042604 <nathan.ferre@ynov.com>
2026-05-25 10:53:26 +0200

    fix(comments): require non-empty content with 2000 char max on create and update

--- src/RedPandaFlow.Application/DTOs/Cards/Comments/UpdateCommentRequest.cs
+++ src/RedPandaFlow.Application/DTOs/Cards/Comments/UpdateCommentRequest.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RedPandaFlow.Application.DTOs
 {
     public class UpdateCommentRequest
     {
+        [Required]
+        [StringLength(2000, MinimumLength = 1)]
         public string Content { get; set; } = string.Empty;
     }
 }
